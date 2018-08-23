2f0ccf9b90b434aeb8ce6e5b98b1116310bec30f
wang <essentialva@163.com>
2018-08-23 16:19:13 +0800

    twoSum

--- 1-sum.cpp
+++ 1-sum.cpp
@@ -1,24 +1,49 @@
 /*
-    本题思路：很简单的来一个O2的扫描计算就行了，但是这样会超时间复杂度，所以需要复习的是类似map一类的算法
+    本题思路：很简单的来一个O(N2)的扫描计算就行了，但是这样会超时间复杂度，所以需要复习的是类似map一类的算法
 */
-
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
 class Solution {
 public:
+//以下是低效解法，时间复杂度为O(N2)
     vector<int> twoSum(vector<int>& nums, int target) {
     int indexa=0,indexb=0;
     vector<int> b={};
-    for(int i : nums) {
-		for (int j : nums) {
-			if (indexa != indexb) {
-				if (i + j == target) {
-					b.push_back(indexa);
-					b.push_back(indexb);
-                    return b;
-				}
+		for (indexa; indexa < nums.size(); indexa++)
+		{
+			for (indexb = indexa; indexb < nums.size(); indexb++)
+			{
+					if (nums[indexa] + nums[indexb] == target)
+					{
+						b.push_back(indexa);
+						b.push_back(indexb);
+						return b;
+					}
+				
 			}
-			indexb++;
 		}
-		indexa++;
 	}
-    }
+	//高效解法
+	vector<int> twoSumHigh(vector<int>& nums, int target) {
+		return null;
+	}
 };
+
+int main(){
+	Solution a;
+	vector<int> nums;
+	int num,target;
+	cout << "Please Input nums:" << endl;
+	while(cin>>num){
+		nums.push_back(num);
+	}
+	cout<<"Please input target:";
+	cin >> target;
+	a.twoSum(nums, target);
+
+	system("pause");
+	return 0;
+}
